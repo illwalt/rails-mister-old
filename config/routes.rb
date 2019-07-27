5e3dc402f36b66f6d75ad1f5d471e8e7c3cf268a
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-07-27 17:10:53 +0100

    Mr Cocktail views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:new, :create, :show, :index] do
+    resources :doses, only: [:new, :create, :destroy]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Cocktail.create(name: "rum & lemon")
+Cocktail.create(name: "Whiskey & lemon")
+
+lemon  = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+mint = Ingredient.create(name: "mint leaves")
+coca_cola = Ingredient.create(name: "coca-cola")
+rum = Ingredient.create(name: "rum")
+whiskey = Ingredient.create(name: "whiskey")
+
+Dose.create(description: "6 cl", ingredient: lemon, cocktail: Cocktail.all.sample)
+Dose.create(description: "6 cl", ingredient: ice, cocktail: Cocktail.all.sample)
+Dose.create(description: "6 cl", ingredient: ice, cocktail: Cocktail.all.sample)
+Dose.create(description: "6 cl", ingredient: mint, cocktail: Cocktail.all.sample)
